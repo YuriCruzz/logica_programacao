6ec73622386bf157a47b19af370ff65657266c04
Yuri Cruz <douglasy190@gmail.com>
2023-10-30 15:40:49 -0300

    questão 14 feita

---
+++ lista_exercicios_01/q14.py
@@ -0,0 +1,14 @@
+# Item A
+print('Hello World!')
+
+# Item B
+print('Hello', 'World')
+
+# Item C
+print('Hello!\n My name is John')
+
+# Item D
+print('Hello!\t My name is John')
+
+# Item E
+print('Um dia, ela disse: “Estarei aqui por você”.')
