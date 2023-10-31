cd920d8f89e914021231d6049f215a0a77573d98
Yuri Cruz <douglasy190@gmail.com>
2023-10-31 16:07:06 -0300

    Questões 19 a 25 feitas

---
+++ lista_exercicios_01/q19.py
@@ -0,0 +1,9 @@
+print('''Escolha seu sabor de sorvete favorito:
+      01. Chocolate.
+      02. Morango.
+      03. Balmilha.
+      04. Creme.''')
+
+x = input('Responda: ')
+
+print('Sua resposta;', x)
---
+++ lista_exercicios_01/q25.py
@@ -0,0 +1,9 @@
+# Definição Variavis:
+a = int(input("Nota 1: "))
+b = int(input("Nota 2: "))
+c = int(input("Nota 3: "))
+d = int(input("Nota 4: "))
+diferenca = int(a * b - c * d)
+
+# Definição Codigos:
+print(diferenca)
