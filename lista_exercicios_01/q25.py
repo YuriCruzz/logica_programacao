d9d748340f68ca65ef631322caabeb01b6e0cb36
YuriCruzz <douglasy190@gmail.com>
2023-10-31 23:24:23 -0300

    aa

--- lista_exercicios_01/q25.py
+++ lista_exercicios_01/q25.py
@@ -1,8 +1,8 @@
 # Definição Variavis:
-a = int(input("Nota 1: "))
-b = int(input("Nota 2: "))
-c = int(input("Nota 3: "))
-d = int(input("Nota 4: "))
+a = int(input("Variavel 1: "))
+b = int(input("Variavel 2: "))
+c = int(input("Variavel 3: "))
+d = int(input("Variavel 4: "))
 diferenca = int(a * b - c * d)
 
 # Definição Codigos:
