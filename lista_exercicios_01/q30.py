9b42eebd3026e6c1366ec12d24641aa49dca1b6c
Yuri Cruz <douglasy190@gmail.com>
2023-11-06 16:20:47 -0300

    30 e 31 não finalizadas, rever

---
+++ lista_exercicios_01/q30.py
@@ -0,0 +1,6 @@
+# Definição Variavis:
+a = input('Defina a variavel A; ')
+b = input('Defina a variavel B; ')
+c = input('Defina a variavel C; ')
+
+# Definição Codigos:
---
+++ lista_exercicios_01/q31.py
@@ -0,0 +1,5 @@
+# Definição Variavis:
+
+
+# Definição Codigos:
+print(10 % 3 * 10 ** 2 + 1 - 10 * 4 / 2)
